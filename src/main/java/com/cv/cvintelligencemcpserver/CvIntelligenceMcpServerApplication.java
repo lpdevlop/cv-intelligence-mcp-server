c790d28d4a18731d7a9775250ed14e5d20bf030e
lasithpalihawadana <lasithpalihawadana@gmail.com>
2026-04-25 20:13:12 +0530

    first commit

---
+++ src/main/java/com/cv/cvintelligencemcpserver/CvIntelligenceMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.cv.cvintelligencemcpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvIntelligenceMcpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvIntelligenceMcpServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cv/cvintelligencemcpserver/CvIntelligenceMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cv.cvintelligencemcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvIntelligenceMcpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
